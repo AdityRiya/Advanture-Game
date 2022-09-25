4e9b5b28245fa07c210643eb857e42ee722f3f4c
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-09-25 22:09:54 +0600

    update

--- Advanture_game.py
+++ Advanture_game.py
@@ -5,6 +5,26 @@ answer = input('Your are in Nstu Main Gate. Do You want to go in?(Yes/No) ').low
 if answer == "yes":
     print("Now you can see the Proshashonik Bluiding , Academic 1 , Academic 2 and Auditorium .")
     answer = input("Want to go right or left? ").lower()
+    if answer == "right":
+        print("Now you can see neel dighi , and in the left you can see Library ")
+        answer = input("Do you wanna go  forward?" ).lower()
+        if answer == "yes":
+            print("here you can see 2 female hall of nstu")
+        elif answer == "no":
+            print("Tour is over.")
+        else: 
+        print("Not a valid answer.")
+    elif answer == "left":
+        print("here you can see Two Male Hall of the nstu and one Female hall of the nstu.") 
+        answer = input("Do you wanna go  forward?" ).lower()
+        if answer == "yes":
+            print("here you can see 2 female hall of nstu")
+        elif answer == "no":
+            print("Tour is over.") 
+        else: 
+        print("Not a valid answer.")
+    else: 
+        print("Not a valid answer.")
 elif answer == "no":
     print("The Tour is over.")
 else:
