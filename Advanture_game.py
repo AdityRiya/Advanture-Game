95fd26af367b39a66074f0f5d9799b67a0f189e2
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-09-25 22:00:53 +0600

    update 2

--- Advanture_game.py
+++ Advanture_game.py
@@ -3,4 +3,9 @@ print('Welcome,',name ,'to the game. ')
 print("let's start the game.")
 answer = input('Your are in Nstu Main Gate. Do You want to go in?(Yes/No) ').lower()
 if answer == "yes":
-    print("")
+    print("Now you can see the Proshashonik Bluiding , Academic 1 , Academic 2 and Auditorium .")
+    answer = input("Want to go right or left? ").lower()
+elif answer == "no":
+    print("The Tour is over.")
+else:
+    print("Not a valid answer.")
