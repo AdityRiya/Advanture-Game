6c160603ecc0c6acf02a0e7215e7721fe291c2f9
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-09-25 21:55:13 +0600

    update

---
+++ Advanture_game.py
@@ -0,0 +1,6 @@
+name = input("What's your name? ")
+print('Welcome,',name ,'to the game. ')
+print("let's start the game.")
+answer = input('Your are in Nstu Main Gate. Do You want to go in?(Yes/No) ').lower()
+if answer == "yes":
+    print("")
